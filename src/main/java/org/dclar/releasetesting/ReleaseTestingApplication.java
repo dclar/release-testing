1c8be9cc8ce7a74f897059a29ee447eeab97aec6
yanglu <yanglu@lionbridgecapital.cn>
2019-06-26 23:54:38 +0800

    first commit

---
+++ src/main/java/org/dclar/releasetesting/ReleaseTestingApplication.java
@@ -0,0 +1,13 @@
+package org.dclar.releasetesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseTestingApplication.class, args);
+	}
+
+}
